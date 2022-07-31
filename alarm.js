de88615b1d19bd683d600baf0fc07ae3ce824a02
Ramiz <Ramizworking@gmail.com>
2022-07-31 16:13:27 +0500

    Add validation to input

--- alarm.js
+++ alarm.js
@@ -1,6 +1,7 @@
 const clockNow = document.querySelector('.clock__now');
 const alarmAddButton = document.querySelector('.add__alarm_button');
-let alarmInput = document.querySelector('#alarmsInput');
+const alarmInput = document.querySelector('#alarmsInput');
+
 let divAlarmAnimation;
 let tagSound;
 let date;
@@ -10,6 +11,8 @@ let seconds;
 let onAlarm;
 let isPressBackspace = false;
 let arrayInputData = [];
+let numOne;
+let numTwo;
 
 function actualTime() {
     date = new Date();
@@ -54,18 +57,32 @@ alarmAddButton.onclick = function () {
 
 const onKeyPress = function (event) {
     isPressBackspace = event.keyCode === 8;
+
     if (event.keyCode < 58 && event.keyCode > 46 || isPressBackspace) {
         return true;
     } else {
         event.preventDefault();
         return false;
     }
+
 }
 
 const onInput = function (event) {
     let isRun = true;
     let value = event.target.value;
-    if(arrayInputData.length){
+
+    if (value.length === 2 && value[0] + value[1] >= 24) {
+        alert('Не иожет быть больше 24');
+        value = value[0];
+    }
+
+    if (value.length === 5 && value[3] + value[4] >= 60) {
+        alert('Не иожет быть 60');
+        value = value.slice(0, 4);
+        event.target.value = value;
+    }
+
+    if (arrayInputData.length) {
         for (let i = 2; i < arrayInputData.length; i++) {
             if (arrayInputData[i] === value[i]) {
                 isRun = false;
@@ -74,21 +91,18 @@ const onInput = function (event) {
         }
     }
 
-    if(isRun) {
+    if (isRun) {
         arrayInputData.length = 0;
 
-    for (let i = 0; i < value.length; i++) {
-        arrayInputData.push(value[i]);
-        if (arrayInputData.length === 2 && !isPressBackspace) {
-            arrayInputData.push(':');
+        for (let i = 0; i < value.length; i++) {
+            arrayInputData.push(value[i]);
+
+            if (arrayInputData.length === 2 && !isPressBackspace) {
+                arrayInputData.push(':');
+            }
         }
-
-    }
-
-    event.target.value = arrayInputData.join('');
-    event.preventDefault();
-    } else {
-        console.log('Fucking shit')
+        event.target.value = arrayInputData.join('');
+        event.preventDefault();
     }
 }
 
